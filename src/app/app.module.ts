b597f115075d49b6fa87b7c6cfb754234798aeed
danilagolygin <t0j8kzqboh@gmail.com>
2020-04-26 17:48:45 +0300

    Сделал маршрутизацию и добавил header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+const appRoutes: Routes = [
+  {path: '', component: CarComponent},
+  {path: 'about', component: ContactsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule. forRoot(appRoutes) ,
+    /*Routes*/
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,9 +7,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  speed: number;
+  model: string;
+  colors: Colors;
+  options: string [];
+  isEdit: boolean = false;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.name = 'Audi';
+    this.speed = 235;
+    this.model = 'A8';
+    this.colors = {
+      car: 'White',
+      salon: 'Red',
+      wheels: 'Carbon'
+    };
+    this.options = ['ABS', 'TractionControl', 'Carplay'];
+  }
+
+  showEdit() {
+    this.isEdit = !this.isEdit;
+  }
+
+  addOpt(option) {
+    this.options.unshift(option);
+    return false;
+  }
+
+  deleteOption(index: number): void {
+    this.options.splice(index, 1);
+  }
+
+  carSelect(carName) {
+    if (carName == 'BMW') {
+      this.name = 'BMW';
+      this.speed = 250;
+      this.model = '325i';
+      this.colors = {
+        car: 'Gray',
+        salon: 'Black',
+        wheels: 'Silver'
+      };
+      this.options = ['ABS', 'TractionControl'];
+    } else if (carName == 'Audi') {
+      this.name = 'Audi';
+      this.speed = 235;
+      this.model = 'A8';
+      this.colors = {
+        car: 'White',
+        salon: 'Red',
+        wheels: 'Carbon'
+      };
+      this.options = ['ABS', 'TractionControl', 'Carplay'];
+    } else if (carName == 'MB') {
+      this.name = 'Mercedes-Benz';
+      this.speed = 250;
+      this.model = 'E63 AMG';
+      this.colors = {
+        car: 'Black',
+        salon: 'White',
+        wheels: 'Black'
+      };
+      this.options = ['ABS', 'TractionControl', 'Carplay'];
+    }
+  }
+
+}
+
+interface Colors {
+  car: string,
+  salon: string,
+  wheels: string
 }
